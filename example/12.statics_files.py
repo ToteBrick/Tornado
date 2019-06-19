638f791ea5185b42c2387d3777a2ab192538c0c2
ToteBrick <402981948@qq.com>
2019-06-19 13:51:22 +0800

    静态文件

---
+++ example/12.statics_files.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+import os
+
+from tornado.web import Application, RequestHandler, StaticFileHandler
+from tornado.ioloop import IOLoop
+
+
+class IndexHandler(RequestHandler):
+
+    def get(self):
+        self.write('index')
+        print(os.path.dirname(__file__))
+
+
+if __name__ == "__main__":
+    current_path = os.path.dirname(__file__)
+    app = Application(
+        [
+            (r"/", IndexHandler),
+            (r'^/()$', StaticFileHandler,
+             {"path": os.path.join(current_path, "statics/html"), "default_filename": "index.html"}),
+            (r'^/view/(.*)$', StaticFileHandler, {"path": os.path.join(current_path, "statics/html")}),
+        ],
+        static_path=os.path.join(current_path, "statics"),
+
+    )
+
+    app.listen(8001)
+    IOLoop.current().start()
